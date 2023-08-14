502206a231f8b76044e43762dc2dc1b5e50c3d08
Pema-Rinchen <111570488+Pema-Rinchen@users.noreply.github.com>
2023-08-14 08:24:14 +0530

    Create link list.c

---
+++ link list.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure of a Node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to insert a new node at the end of the linked list
+void append(struct Node** head_ref, int new_data) {
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->next = NULL;
+
+    if (*head_ref == NULL) {
+        *head_ref = new_node;
+        return;
+    }
+
+    struct Node* last = *head_ref;
+    while (last->next != NULL)
+        last = last->next;
+
+    last->next = new_node;
+}
+
+// Function to print the linked list
+void printList(struct Node* node) {
+    while (node != NULL) {
+        printf("%d -> ", node->data);
+        node = node->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    append(&head, 1);
+    append(&head, 2);
+    append(&head, 3);
+    append(&head, 4);
+    append(&head, 5);
+
+    printf("Linked List: ");
+    printList(head);
+
+    return 0;
+}
