1cf719da2cdf7fada7e97ad0bbbfc85e27f0136c
Pema Rinchen <111570488+Pema-Rinchen@users.noreply.github.com>
2023-09-17 11:13:44 +0530

    Create Double Link list insertion in beginning.c

---
+++ Double Link list insertion in beginning.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+struct Node* addToBeginning(struct Node* head, int data) {
+     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    if (head == NULL) {
+        return newNode;
+    } else {
+        newNode->next = head;
+        head->prev = newNode;
+        return newNode;
+    }
+}
+int main() {
+    struct Node* head = NULL;
+    int array[5] = {5, 4, 3, 2, 1};
+    for (int i = 0; i < 5; i++) {
+        head = addToBeginning(head, array[i]);
+    }
+
+    printf("Double-Linked List: ");
+     struct Node* current = head;
+    while (current != NULL) {
+        printf("%d <-> ", current->data);
+        current = current->next;
+    }
+    return 0;
+}
