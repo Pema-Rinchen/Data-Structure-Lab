88a485404d1fe5cb1d6fde14da77dda1de29bba2
Pema Rinchen <111570488+Pema-Rinchen@users.noreply.github.com>
2023-10-23 09:45:42 +0530

    Create 2-c.c

---
+++ experiment/2-c.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void insertion(int arr[],int size){
+	int i,j,key;
+	for (i=1;i<size;i++){
+		key=arr[i];
+		j=i-1;
+		while(j>=0 && arr[j]>key){
+		arr[j+1]=arr[j];
+		j--;
+		}
+		arr[j+1]=key;
+	}
+}
+int main() {
+    int arr[] = {1,4,5,2,3};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    printf("Original array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    insertion(arr,size);
+    printf("\nSorted array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
